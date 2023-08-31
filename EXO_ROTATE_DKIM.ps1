572505e45777667ce0601369ac246ba0b9e7cf38
IntuneAdmin <93092915+IntuneAdmin@users.noreply.github.com>
2023-08-31 21:37:23 +0200

    Add files via upload

---
+++ EXO_ROTATE_DKIM.ps1
@@ -0,0 +1,8 @@
+$organization = "Cloud23nl.onmicrosoft.com"
+Connect-ExchangeOnline -ManagedIdentity -Organization $organization
+
+$customdomains = (Get-DkimSigningConfig | ? { $_.Domain -NotLike '*.onmicrosoft.com' -and $_.Enabled -like 'True'}).name 
+$customdomains | foreach { 
+$Rotate = Rotate-DkimSigningConfig -Identity $_ 
+}
+
