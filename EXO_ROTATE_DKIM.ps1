be1542103bfe7d646a0e1e17a6d574768b3a73ca
IntuneAdmin <93092915+IntuneAdmin@users.noreply.github.com>
2023-08-31 21:38:22 +0200

    Update EXO_ROTATE_DKIM.ps1

--- EXO_ROTATE_DKIM.ps1
+++ EXO_ROTATE_DKIM.ps1
@@ -1,4 +1,4 @@
-$organization = "Cloud23nl.onmicrosoft.com"
+$organization = "<DOMAINNAME>.onmicrosoft.com"
 Connect-ExchangeOnline -ManagedIdentity -Organization $organization
 
 $customdomains = (Get-DkimSigningConfig | ? { $_.Domain -NotLike '*.onmicrosoft.com' -and $_.Enabled -like 'True'}).name 
